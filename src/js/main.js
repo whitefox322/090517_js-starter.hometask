c9dd64d56bdd0c1fa004e2b1733246a38372de89
Roman Tsvyk <roma.tsvyk@gmail.com>
2017-05-09 13:43:47 +0300

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+var firstNameInput = prompt ("Enter you first name:");
+
+var firstNameOutput = document.getElementById("firstName");
+firstNameOutput.innerText = firstNameInput;
+
+var familyNameInput = prompt ("Enter you family name:");
+
+var familyNameOutput = document.getElementById("familyName");
+familyNameOutput.innerText = familyNameInput;
+
+var ageInput = prompt ("Enter you age:");
+
+var ageOutput = document.getElementById("age");
+ageOutput.innerText = ageInput;
+
+var countryInput = prompt ("Enter you country:");
+
+var countryOutput = document.getElementById("country");
+countryOutput.innerText = countryInput;
+
+var emailInput = prompt ("Enter you E-mail:");
+
+var emailOutput = document.getElementById("email");
+emailOutput.innerText = emailInput;
